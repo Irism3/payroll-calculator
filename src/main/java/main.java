943c7689b44cd345c80433a6338f413b9bf96efd
irismelendez <iris.13melendez@gmail.com>
2025-09-25 12:51:25 -0500

    Exercise 4 - PayrollCalculator

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class main {
+    public static void main(String[] args) {
+        Scanner scanner;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+
+        System.out.print("Enter hours worked: ");
+        double hoursWorked = input.nextDouble();
+
+        System.out.print("Please enter pay rate: ");
+        double payRate = input.nextDouble();
+
+        double grossPay = hoursWorked * payRate;
+
+        //System.out.println("name "+ name);
+       // System.out.println("grossPay "+ grossPay);
+        input.close();
+
+        System.out.print("name "+ name + " grossPay "+ grossPay);
+
+
+
+
+
+
+    }
 }
